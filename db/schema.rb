55e4ae6501ce9610d734ed0b7ad7dce1c7b80fea
Sharath <sharathnagalikar@gmail.com>
2017-08-30 17:38:40 +0530

    scaffloded the author and added validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830114240) do
+ActiveRecord::Schema.define(version: 20170830120632) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170830114240) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
